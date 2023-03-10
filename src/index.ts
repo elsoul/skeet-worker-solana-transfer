b86515da4ba49e81eff978bb86e5fba08f1b798e
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-03-10 09:58:48 +0100

    add solana transfer

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,6 @@ router.post('/run', Endpoint.run)
 app.use('/', router)
 
 export const expressServer = app.listen(port, async () => {
-  console.log(process.env.SKEET_CRYPTO_SALT)
   console.log(
     `[env:${skeetEnv}]: Server is running at http://localhost:${port}`
   )
