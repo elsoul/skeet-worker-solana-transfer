0d71010dc6c50ae849ca63f2c8c61855950b0c0b
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-03-14 11:01:22 +0100

    update task

--- src/lib/createCloudTask.ts
+++ src/lib/createCloudTask.ts
@@ -1,49 +1,50 @@
 import { v2 } from '@google-cloud/tasks'
 import fetch from 'node-fetch'
 import { SolanaTransferResponseParam } from '@/types/api/SolanaTransferParam'
+import encodeBase64 from '@/utils/base64'
 
 const { CloudTasksClient } = v2
 const project = process.env.SKEET_GCP_PROJECT_ID || 'skeet-framework'
 const location = process.env.SKEET_GCP_TASK_REGION || 'europe-west1'
-const API_URL = process.env.SKEET_API_URL || ''
+const API_ENDPOINT_URL = process.env.SKEET_API_ENDPOINT_URL || ''
 const API_DEV_URL = 'http://host.docker.internal:4000/graphql'
-
-export enum GraphQLMethod {
-  query = 'query',
-  mutation = 'mutation',
-}
 
 export const createCloudTask = async (
   queue: string,
   mutationName: string,
   params: SolanaTransferResponseParam
 ) => {
-  const body = await genGraphqlRequest(mutationName, params)
-  const client = new CloudTasksClient()
-  async function createTask() {
-    const parent = client.queuePath(project, location, queue)
-    const task = {
-      httpRequest: {
-        headers: {
-          'Content-Type': 'application/json',
+  try {
+    const graphql = await genGraphqlRequest(mutationName, params)
+    const body = await encodeBase64(graphql)
+    const client = new CloudTasksClient()
+    async function createTask() {
+      const parent = client.queuePath(project, location, queue)
+      const task = {
+        httpRequest: {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          httpMethod: 'POST',
+          url: API_ENDPOINT_URL,
+          body,
         },
-        httpMethod: 'POST',
-        url: API_URL,
-        body,
-      },
+      }
+
+      console.log('Sending task:')
+
+      // Send create task request.
+      const request = { parent: parent, task: task }
+      //@ts-ignore
+      const [response] = await client.createTask(request)
+      const name = response.name
+      console.log(`Created task ${name}`)
     }
 
-    console.log('Sending task:')
-
-    // Send create task request.
-    const request = { parent: parent, task: task }
-    //@ts-ignore
-    const [response] = client.createTask(request)
-    const name = response.name
-    console.log(`Created task ${name}`)
+    createTask()
+  } catch (error) {
+    console.log(`createCloudTask: ${error}`)
   }
-
-  createTask()
 }
 
 export const genGraphqlRequest = async (
