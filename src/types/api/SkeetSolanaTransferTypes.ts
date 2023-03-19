4beadb057854d3243af9a84790968f904b787045
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-03-19 16:07:01 +0100

    update params

--- src/types/api/SkeetSolanaTransferTypes.ts
+++ src/types/api/SkeetSolanaTransferTypes.ts
@@ -1,4 +1,5 @@
 export type SkeetSolanaTransferParam = {
+  id?: number
   toAddressPubkey: string
   transferAmountLamport: number
   tokenMintAddress: string
@@ -9,6 +10,7 @@ export type SkeetSolanaTransferParam = {
 }
 
 export type SkeetSolanaTransferResponse = {
+  id?: number
   toAddressPubkey: string
   fromAddressPubkey: string
   transferAmountLamport: number
